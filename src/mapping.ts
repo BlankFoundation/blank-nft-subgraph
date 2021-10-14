0cf5f9258ba40feda3656042473c29eaed043934
Ford <ford@thegraph.com>
2021-10-13 20:03:33 -0700

    *: Handle Minted & TokenUriUpdated events, add Token & Account entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,18 +4,84 @@ import {
   FoundationAddressUpdated as FoundationAddressUpdatedEvent,
   MemberAdded as MemberAddedEvent,
   MemberRevoked as MemberRevokedEvent,
+  Minted as MintedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
+  TokenUriUpdated as TokenUriUpdatedEvent,
   Transfer as TransferEvent
 } from "../generated/BlankArt/BlankArt"
 import {
+  Account,
   Approval,
   ApprovalForAll,
   FoundationAddressUpdated,
-  MemberAdded,
-  MemberRevoked,
   OwnershipTransferred,
-  Transfer
+  Token
 } from "../generated/schema"
+
+export function handleMemberAdded(event: MemberAddedEvent): void {
+  let memberAddressString = event.params.member.toHexString()
+
+  let member = Account.load(memberAddressString)
+  if(member == null) {
+    member = new Account(memberAddressString)
+  }
+  member.address = event.params.member
+  member.status = "Active"
+  member.save()
+}
+
+export function handleMemberRevoked(event: MemberRevokedEvent): void {
+  let memberAddressString = event.params.member.toHexString()
+
+  let member = Account.load(memberAddressString)
+  if(member !== null) {
+    member.address = event.params.member
+    member.status = "Revoked"
+    member.save()
+  }
+}
+
+export function handleMinted(event: MintedEvent): void {
+  let token = new Token(
+    event.params.tokenId.toString()
+  )
+  let memberAddressString = event.params.member.toHexString()
+  token.tokenId = event.params.tokenId
+  token.owner = memberAddressString
+  token.tokenURI = event.params.tokenURI
+  token.save()
+
+  let member = Account.load(memberAddressString)
+  if(member == null) {
+    member = new Account(memberAddressString)
+  }
+  member.address = event.params.member
+  member.status = "Active"
+  member.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let newOwner = event.params.to.toString()
+  let oldOwner = event.params.from.toString()
+
+  let token = Token.load(event.params.tokenId.toString())
+  token.owner = newOwner
+  token.save()
+
+  let member = Account.load(newOwner)
+  if(member == null) {
+    member = new Account(newOwner)
+  }
+  member.address = event.params.to
+  member.status = "Active"
+  member.save()
+}
+
+export function handleTokenUriUpdated(event: TokenUriUpdatedEvent): void {
+  let token = Token.load(event.params.tokenId.toString())
+  token.tokenURI = event.params.tokenURI
+  token.save()
+}
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
@@ -47,22 +113,6 @@ export function handleFoundationAddressUpdated(
   entity.save()
 }
 
-export function handleMemberAdded(event: MemberAddedEvent): void {
-  let entity = new MemberAdded(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.member = event.params.member
-  entity.save()
-}
-
-export function handleMemberRevoked(event: MemberRevokedEvent): void {
-  let entity = new MemberRevoked(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.member = event.params.member
-  entity.save()
-}
-
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
@@ -71,15 +121,5 @@ export function handleOwnershipTransferred(
   )
   entity.previousOwner = event.params.previousOwner
   entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
   entity.save()
 }
